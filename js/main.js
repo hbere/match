3adabda2c63158fac5f96e71bd4ebf545e310e7e
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-06-18 18:44:36 -0400

    Add card click event listeners

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 // Memory Game Project JavaScript file
 
+// Event listeners for DOM
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('the DOM is ready to be interacted with!');
+});
+
+// Event listeners for cards
+let cards = document.querySelectorAll('.card');
+for (let card of cards) {
+    card.addEventListener('click', function clicked() {
+        console.log(`A card was clicked with text == ${card.textContent}`);
+        card.classList.toggle('clicked');
+        // TODO add logic for waiting & turning blue again if no match
+        // TODO add logic for waiting & turning green if match
+        // TODO add logic to count total # matches and restarting the game if all matches found
+    });
+}
+
