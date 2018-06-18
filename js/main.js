90a56420d099a60b5c5d9fe87c7873c52f0ef5d9
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-06-18 18:55:24 -0400

    Center text on cards

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 // Memory Game Project JavaScript file
+let cardText = [
+  "A",
+  "A",
+  "B",
+  "B",
+  "C",
+  "C",
+  "D",
+  "D",
+  "E",
+  "E",
+  "F",
+  "F",
+  "G",
+  "G",
+  "H",
+  "H"
+];
 
 // Event listeners for DOM
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('the DOM is ready to be interacted with!');
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("the DOM is ready to be interacted with!");
 });
 
 // Event listeners for cards
-let cards = document.querySelectorAll('.card');
+let cards = document.querySelectorAll(".card");
 for (let card of cards) {
-    card.addEventListener('click', function clicked() {
-        console.log(`A card was clicked with text == ${card.textContent}`);
-        card.classList.toggle('clicked');
-        // TODO add logic for waiting & turning blue again if no match
-        // TODO add logic for waiting & turning green if match
-        // TODO add logic to count total # matches and restarting the game if all matches found
-    });
+  card.addEventListener("click", function clicked() {
+    console.log(`A card was clicked with text == ${card.textContent}`);
+    card.classList.toggle("clicked");
+    // TODO add logic for waiting & turning blue again if no match
+    // TODO add logic for waiting & turning green if match
+    // TODO add logic to count total # matches and restarting the game if all matches found
+  });
 }
 
+// TODO add function to randomize card order
