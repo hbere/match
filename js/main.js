5208ec0b695e3f363dd726a218050880062b5f2b
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-06-19 11:20:14 -0400

    Add deal function

--- js/main.js
+++ js/main.js
@@ -17,48 +17,53 @@ let deck = [
   "H",
   "H"
 ];
-let shuffledDeck = new Array(deck.length);
 
 // Event listeners for DOM
 document.addEventListener("DOMContentLoaded", function() {
   // console.log("the DOM is ready to be interacted with!");
-  shuffle();
+  addCardEventListeners();
+  console.log(deck);
+  shuffle(deck);
+  console.log(deck);
+  deal(deck);
 });
 
 // Event listeners for cards
-let cards = document.querySelectorAll(".card");
-for (let card of cards) {
-  card.addEventListener("click", function clicked() {
-    console.log(`A card was clicked with text == ${card.textContent}`);
-    card.classList.toggle("clicked");
-    // TODO add logic for waiting & turning blue again if no match
-    // TODO add logic for waiting & turning green if match
-    // TODO add logic to count total # matches and restarting the game if all matches found
-  });
+function addCardEventListeners() {
+  let cards = document.querySelectorAll(".card");
+  for (let card of cards) {
+    card.addEventListener("click", function clicked() {
+      console.log(`A card was clicked with text == ${card.textContent}`);
+      card.classList.toggle("clicked");
+      // TODO add logic for waiting & turning blue again if no match
+      // TODO add logic for waiting & turning green if match
+      // TODO add logic to count total # matches and restarting the game if all matches found
+    });
+  }
 }
 
-// TODO add function to randomize card order
-function shuffleIt(array) {
-  // Declare variables
-  let tempArray = [], tempIndex, shuffledArray;
-  // Generate numbers 0 to deck length minus one
-  for (let i = 0; i < deck.length; i++) {
-    tempArray[i] = i;
+function deal(array) {
+  // Deals cards to the UI
+  let cards = document.querySelectorAll(".card");
+  let i = 0;
+  for (let card of cards) {
+    card.textContent = array[i];
+    i++;
   }
-  // Get new deck order
-  for (let i = 0; i < deck.length; i++) {
-    console.log(tempArray);
-    // Choose number from 0 to deck length minus one, without replacement
-    console.log(tempArray.length-1);
-    tempIndex = tempArray[getRandomInt(0, tempArray.length-1)];
-    console.log(tempIndex);
-    // push from tempArray onto shuffledDeck
-    shuffledDeck[i] = deck[tempIndex];
-    // pop from tempArray
-    tempArray.splice(tempIndex, 1);
+}
+
+function shuffle(array) {
+  // Performs as Fisher-Yates shuffle
+  // More info @ https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+  let maxIndex, randIndex;
+  // Shuffle
+  for (let [index] of array.entries()) {
+    maxIndex = array.length - 1;
+    randIndex = getRandomInt(0, maxIndex);
+    [array[randIndex], array[index]] = [array[index], array[randIndex]];
   }
-  console.log(shuffledDeck);
-  return shuffledArray;
+  // Return shuffled array
+  return array;
 }
 
 function getRandomInt(min, max) {
