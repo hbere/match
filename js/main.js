556caf41a16a1f2c1dc6d18938034d14f3b1e4aa
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-06-19 10:43:17 -0400

    Begin shuffle algorithm attempt

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Memory Game Project JavaScript file
-let cardText = [
+let deck = [
   "A",
   "A",
   "B",
@@ -17,10 +17,12 @@ let cardText = [
   "H",
   "H"
 ];
+let shuffledDeck = new Array(deck.length);
 
 // Event listeners for DOM
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("the DOM is ready to be interacted with!");
+  // console.log("the DOM is ready to be interacted with!");
+  shuffle();
 });
 
 // Event listeners for cards
@@ -36,3 +38,32 @@ for (let card of cards) {
 }
 
 // TODO add function to randomize card order
+function shuffleIt(array) {
+  // Declare variables
+  let tempArray = [], tempIndex, shuffledArray;
+  // Generate numbers 0 to deck length minus one
+  for (let i = 0; i < deck.length; i++) {
+    tempArray[i] = i;
+  }
+  // Get new deck order
+  for (let i = 0; i < deck.length; i++) {
+    console.log(tempArray);
+    // Choose number from 0 to deck length minus one, without replacement
+    console.log(tempArray.length-1);
+    tempIndex = tempArray[getRandomInt(0, tempArray.length-1)];
+    console.log(tempIndex);
+    // push from tempArray onto shuffledDeck
+    shuffledDeck[i] = deck[tempIndex];
+    // pop from tempArray
+    tempArray.splice(tempIndex, 1);
+  }
+  console.log(shuffledDeck);
+  return shuffledArray;
+}
+
+function getRandomInt(min, max) {
+  // return 1 random integer between min and max
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
